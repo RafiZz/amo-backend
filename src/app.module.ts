f07beb93a6f837e53fb45063976740921743c66e
Rafis Bikbov <dribblerafis@gmail.com>
2023-03-27 23:09:43 +0300

    unused code removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { LeadsController } from './api/v1/leads/leads.controller';
 import { LeadsService } from './api/v1/leads/leads.service';
 import { HttpModule } from '@nestjs/axios';
@@ -8,7 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [ConfigModule.forRoot(), HttpModule],
-  controllers: [AppController, LeadsController],
-  providers: [AppService, LeadsService],
+  controllers: [LeadsController],
+  providers: [LeadsService],
 })
 export class AppModule {}
