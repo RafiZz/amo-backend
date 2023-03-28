4c49ebc3ef70b14adab1e842df0a11896410c443
Rafis Bikbov <dribblerafis@gmail.com>
2023-03-28 13:21:28 +0300

    CORS allowed for all origins

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+    },
+  });
   const port = parseInt(process.env.PORT, 10) || 3000;
   await app.listen(port, () => {
     console.log(`Port: ${port}`);
